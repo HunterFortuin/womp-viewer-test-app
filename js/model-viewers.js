a34fef67b0ce2604ce012761ab46638c4cda1370
unilimes <unilimesdevs@gmail.com>
2018-10-11 14:40:11 +0300

    - Fix problem with Faces - Add Function for changing maps - Add Function for changing material - White Light and Env

--- js/model-viewers.js
+++ js/model-viewers.js
@@ -1,81 +1,102 @@
-$(document).ready(function() {
-  function onWindowResize() {
-  	camera.aspect = window.innerWidth / window.innerHeight;
-  	camera.updateProjectionMatrix();
+var scene
+$(document).ready(function () {
+	function onWindowResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
 
-  	renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setSize(window.innerWidth, window.innerHeight);
 
-  	controls.handleResize();
-  	controls.update();
-  	renderer.render( scene, camera );
-  }
+		controls.handleResize();
+		controls.update();
+		renderer.render(scene, camera);
+	}
 
-  var redraw = true;
-  function animate() {
-  	requestAnimationFrame(animate);
+	var redraw = true;
 
-  	controls.update();
-  	if(redraw)
-  		renderer.render( scene, camera );
-  	redraw = false;
-  }
+	function animate() {
+		requestAnimationFrame(animate);
 
-  var camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.1, 100 );
-  camera.position.z = 4;
+		controls.update();
+		if (redraw)
+			renderer.render(scene, camera);
+		redraw = false;
+	}
 
-  var controls = new THREE.TrackballControls( camera );
-  controls.rotateSpeed = 10.0;
-  controls.zoomSpeed = 1.5;
-  controls.panSpeed = 0.8;
-  controls.noZoom = false;
-  controls.noPan = false;
-  controls.staticMoving = true;
-  controls.dynamicDampingFactor = 0.3;
-  controls.keys = [ 65, 83, 68 ];
-  controls.addEventListener( 'change', function() { redraw = true; } );
+	var camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera.position.z = 50;
 
-  var scene = new THREE.Scene();
-  scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
-  scene.add( new THREE.AmbientLight( 0x444444 ) );
+	var controls = new THREE.TrackballControls(camera);
+	controls.rotateSpeed = 10.0;
+	controls.zoomSpeed = 1.5;
+	controls.panSpeed = 0.8;
+	controls.noZoom = false;
+	controls.noPan = false;
+	controls.staticMoving = true;
+	controls.dynamicDampingFactor = 0.3;
+	controls.keys = [65, 83, 68];
+	controls.addEventListener('change', function () {
+		redraw = true;
+	});
 
-  var light1 = new THREE.DirectionalLight( 0xffffff, 1.0 );
-  light1.position.set( 1, 1, -1 );
-  scene.add( light1 );
-
-  var light2 = new THREE.DirectionalLight( 0xffffff, 1.0 );
-  light2.position.set( -1, -1, 1 );
-  scene.add( light2 );
-
-  var renderer = new THREE.WebGLRenderer( { antialias: false } );
-  renderer.setClearColor( scene.fog.color );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight);
-
-  var viewer = document.getElementById( 'js-viewer-container');
-  viewer.appendChild( renderer.domElement );
-
-  var model = viewer.dataset.link;
-
-  /* An appropriate material can be used as a fourth arg for the NexusObject constructor
-
-  var texture = new THREE.DataTexture( new Uint8Array([1, 1, 1]), 1, 1, THREE.RGBFormat );
-  texture.needsUpdate = true;
-  var material = new THREE.MeshLambertMaterial( { color: 0xffffff, map: texture } );
-  */
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0xffffff);
+	scene.fog = new THREE.Fog(0x050505, 2000, 3500);
+	scene.add(new THREE.AmbientLight(0x444444));
 
 
-  function onNexusLoad() {
-  	var s   = 1/nexus_obj.geometry.boundingSphere.radius;
-  	var p = nexus_obj.geometry.boundingBox.getCenter().negate();
-  	nexus_obj.position.set(p.x*s, p.y*s, p.y*s); //.set(p.x, p.y, p.z); // = p; //.set(p.x, p.y, p.z);
-  	nexus_obj.scale.set(s, s, s);
-  	redraw = true;
-  }
+	var geometry = new THREE.SphereGeometry(5, 32, 32);
+	var material = new THREE.MeshBasicMaterial({color: 0xffff00});
+	var sphere = new THREE.Mesh(geometry, material);
+	scene.add(sphere);
 
-  var nexus_obj = new NexusObject(model, onNexusLoad, function() { redraw = true; }, renderer);
-  scene.add(nexus_obj);
+	var light1 = new THREE.DirectionalLight(0xffffff, 1.0);
+	light1.position.set(1, 1, -1);
+	scene.add(light1);
 
-  window.addEventListener( 'resize', onWindowResize, false );
+	var light2 = new THREE.DirectionalLight(0xffffff, 1.0);
+	light2.position.set(-1, -1, 1);
+	scene.add(light2);
 
-  animate();
+	var renderer = new THREE.WebGLRenderer({antialias: false});
+	renderer.setClearColor(scene.fog.color);
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	var viewer = document.getElementById('js-viewer-container');
+	viewer.appendChild(renderer.domElement);
+
+	var model = viewer.dataset.link;
+
+	/* An appropriate material can be used as a fourth arg for the NexusObject constructor
+
+	 var texture = new THREE.DataTexture( new Uint8Array([1, 1, 1]), 1, 1, THREE.RGBFormat );
+	 texture.needsUpdate = true;
+	 var material = new THREE.MeshLambertMaterial( { color: 0xffffff, map: texture } );
+	 */
+
+
+	function onNexusLoad() {
+		var s = 1 / nexus_obj.geometry.boundingSphere.radius;
+		var p = nexus_obj.geometry.boundingBox.getCenter().negate();
+		nexus_obj.position.set(p.x * s, p.y * s, p.y * s); //.set(p.x, p.y, p.z); // = p; //.set(p.x, p.y, p.z);
+		nexus_obj.scale.set(s, s, s);
+		redraw = true;
+	}
+
+	var nexus_obj = new NexusObject(model, onNexusLoad, function () {
+		redraw = true;
+	}, renderer);
+	scene.add(nexus_obj);
+
+	console.log('NEXUS', nexus_obj);
+
+	var box = new THREE.BoxHelper( nexus_obj, 0xffff00 );
+	scene.add( box );
+
+	var bbox = new THREE.Box3().setFromObject(nexus_obj);
+	console.log('BOXXX',bbox);
+
+	window.addEventListener('resize', onWindowResize, false);
+
+	animate();
 });
